09985559e1824045957f8ae1d6e0cd7235464c70
suzukiplan <suzukiplan.ys@gmail.com>
2018-02-17 23:01:23 +0900

    initial commit

---
+++ NESView/NESView.h
@@ -0,0 +1,19 @@
+//
+//  NESView.h
+//  NESView
+//
+//  Created by Yoji Suzuki on 2018/02/17.
+//  Copyright © 2018年 SUZUKI PLAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NESView.
+FOUNDATION_EXPORT double NESViewVersionNumber;
+
+//! Project version string for NESView.
+FOUNDATION_EXPORT const unsigned char NESViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NESView/PublicHeader.h>
+
+
